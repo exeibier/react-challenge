672fd062b9bfeb184b4287ea5e5873a494a81d2f
AntoRend <antorend19@outlook.com>
2020-05-24 08:40:46 -0500

    react-router-dom added

---
+++ src/pages/PostContent/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default class PostContent extends React.Component {
+  render() {
+    return(
+      <div className="Container">
+        <h1>Title</h1>
+        <h3>Subtitle</h3>
+      </div>
+    )
+  }
+}
