f842f3f27010400de594561feafb8897dbb5ed27
noemiAmaya <jazzamaya13@gmail.com>
2020-05-26 15:46:15 -0500

    function renderposts

--- src/pages/Home/Components/Posts/index.js
+++ src/pages/Home/Components/Posts/index.js
@@ -3,30 +3,72 @@ import './Posts.css'
 export default class HideContent extends Component {
     constructor(props) {
         super(props)
+        this.state = {
+            posts: []
+        }
     };
+
+    componentDidMount() {
+        fetch("http://localhost:8080")
+            .then(response => response.json())
+            .then(({ posts }) => {
+                var postsArray = []
+                for (let key in posts) {
+                    postsArray.push({
+                        key,
+                        title: posts[key]['title'],
+                        description: posts[key]['description'],
+                        author: posts[key]['author'],
+                        date: posts[key]['date'],
+                        estimatedReadtime: posts[key]['estimatedReadtime'],
+                        image: posts[key]['image']
+
+                    })
+                }
+
+                this.setState({
+                    posts: postsArray
+
+                })
+            })
+    }
+
+    renderPosts() {
+        const { posts } = this.state;
+        return posts.map(({ key, title, description, author, date, estimatedReadtime, image }) => {
+            return (<div className='ContainerCard'>
+                <div className='justify-content-start card-container'>
+                    <p className='card-medium'>POST ON MEDIUM {key}</p>
+                    <h1 className='card-title'> {title}</h1>
+                    <p className='card-content'>{description}</p>
+                    <a href='#'><small className='card-author'> {author} in Thought Catalog</small></a>
+                    <div>
+                        <span> <small className='card-date'>{date}</small></span>
+                        <small className='card-read'>{estimatedReadtime}</small>
+                        <i class="far fa-star"></i>
+                        <i class="far fa-bookmark"></i>
+                        <i class="fas fa-ellipsis-h"></i>
+                    </div>
+
+                </div>
+                <div className='justify-content-end'>
+                    <img className="card-img" src={image} />
+                </div>
+            </div>
+            );
+
+        })
+
+    }
+
 
     render() {
         return (
-                <div className='ContainerCard'>
-                    <div className='justify-content-start card-container'>
-                        <p className='card-medium'> POPULAR ON MEDIUM</p>
-                        <h1 className='card-title'> The 5-Year Rule: How to Create the Future You Want</h1>
-                        <p className='card-content'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed .</p>
-                        <a href='#'><small className='card-author'> Shannon Hilson in Thought Catalog</small></a>
-                        <div>
-
-                            <span> <small className='card-date'>May 25</small></span>
-                            <small className='card-read'>3 min read</small>
-                            <i class="far fa-star"></i>
-                            <i class="far fa-bookmark"></i>
-                            <i class="fas fa-ellipsis-h"></i>
-                        </div>
-                        
-                    </div>
-                    <div className='justify-content-end'>
-                            <img className="card-img" src={'https://picsum.photos/200'} />
-                    </div>
-                </div>
-                );
-            }
-        }
+            <div>
+                <ul>
+                    {this.renderPosts()}
+                </ul>
+            </div>
+        );
+    }
+}
