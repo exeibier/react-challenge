61579c455572a71dc61122d259cc137d0f1365a8
noemiAmaya <jazzamaya13@gmail.com>
2020-05-26 16:54:44 -0500

    render posts ok

--- src/pages/Home/Components/Posts/index.js
+++ src/pages/Home/Components/Posts/index.js
@@ -9,19 +9,21 @@ export default class HideContent extends Component {
     };
 
     componentDidMount() {
-        fetch("http://localhost:8080")
+        //fetch("https://medium-api-post.mybluemix.net/articles")
+        fetch("http://localhost:8080/articles")
             .then(response => response.json())
-            .then(({ posts }) => {
+            .then(({ data }) => {
+                console.log(data.articles)
                 var postsArray = []
-                for (let key in posts) {
+                for (let key in data.articles) {
                     postsArray.push({
                         key,
-                        title: posts[key]['title'],
-                        description: posts[key]['description'],
-                        author: posts[key]['author'],
-                        date: posts[key]['date'],
-                        estimatedReadtime: posts[key]['estimatedReadtime'],
-                        image: posts[key]['image']
+                        title: data.articles[key]['title'],
+                        description: data.articles[key]['description'],
+                        author: data.articles[key]['author'],
+                        date: data.articles[key]['date'],
+                        estimatedReadTime: data.articles[key]['estimatedReadTime'],
+                        image: data.articles[key]['image']
 
                     })
                 }
@@ -35,24 +37,24 @@ export default class HideContent extends Component {
 
     renderPosts() {
         const { posts } = this.state;
-        return posts.map(({ key, title, description, author, date, estimatedReadtime, image }) => {
+        return posts.map(({ key, title, description, author, date, estimatedReadTime, image }) => {
             return (<div className='ContainerCard'>
                 <div className='justify-content-start card-container'>
-                    <p className='card-medium'>POST ON MEDIUM {key}</p>
-                    <h1 className='card-title'> {title}</h1>
-                    <p className='card-content'>{description}</p>
-                    <a href='#'><small className='card-author'> {author} in Thought Catalog</small></a>
+                    <p className='card-medium' key={key}>POST ON MEDIUM </p>
+                    <h1 className='card-title' title={title}> </h1>
+                    <p className='card-content' description={description}></p>
+                    <a href='#'><small className='card-author' author={author} > in Thought Catalog</small></a>
                     <div>
-                        <span> <small className='card-date'>{date}</small></span>
-                        <small className='card-read'>{estimatedReadtime}</small>
-                        <i class="far fa-star"></i>
-                        <i class="far fa-bookmark"></i>
-                        <i class="fas fa-ellipsis-h"></i>
+                        <span> <small className='card-date' date={date}></small></span>
+                        <small className='card-read' estimatedReadTime={estimatedReadTime}></small>
+                        <i className="far fa-star"></i>
+                        <i className="far fa-bookmark"></i>
+                        <i className="fas fa-ellipsis-h"></i>
                     </div>
 
                 </div>
                 <div className='justify-content-end'>
-                    <img className="card-img" src={image} />
+                    <img  className="card-img" src={image} />
                 </div>
             </div>
             );
