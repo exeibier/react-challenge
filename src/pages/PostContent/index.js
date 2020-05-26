03c4fbb45b8c26e8e2f182638d702ebe7c04fdde
AntoRend <antorend19@outlook.com>
2020-05-26 05:39:25 -0500

    style and fetch getPost

--- src/pages/PostContent/index.js
+++ src/pages/PostContent/index.js
@@ -1,12 +1,30 @@
 import React from 'react'
 
-import { Container, Row, Col, Image } from 'reactstrap';
-
-import Content from './Content'
+import Content from './Components/Content'
 
 import './PostContent.css'
 
 export default class PostContent extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      Posts: [],
+    }
+  }
+  componentDidMount() {
+    fetch("http://localhost:8082/posts")
+    .then(response => response.json())
+    .then(({data: {Posts}}) => {
+      console.log(Posts)
+      var postsArray = []
+      postsArray.push(Posts)
+      console.log(postsArray)
+      // this.setState({
+      //   Posts: postsArray
+      // })
+    })
+  }
+
   render() {
     return(
       <div>
